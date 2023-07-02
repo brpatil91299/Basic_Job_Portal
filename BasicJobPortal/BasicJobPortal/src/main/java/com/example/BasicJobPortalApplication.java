ffd1f3f2e80160d32c5de9302d1934acd2d74e22
Bhushan Patil <brpatil91299@gmail.com>
2023-07-02 22:44:53 +0530

    bhushan

---
+++ BasicJobPortal/BasicJobPortal/src/main/java/com/example/BasicJobPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicJobPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicJobPortalApplication.class, args);
+	}
+
+}
